cf442ab12560624cd09deff4057125178b56a550
Carolina <carolinac98@hotmail.com>
2019-05-08 20:50:36 -0500

    configure la ruta de enlace con la tienda y configure el servidor

---
+++ index.js
@@ -0,0 +1,42 @@
+// importar el módulo express
+var express = require('express');
+var motorRender = require('express-handlebars');
+
+var fs = require('fs');
+
+// crear la variable app usando express
+var app = express();
+
+// configurar la carpeta public como "pública"
+app.use(express.static('public'));
+
+app.engine('handlebars', motorRender());
+app.set('view engine', 'handlebars');
+
+var productos = [];
+productos.push({
+    titulo: 'nevermine',
+    precio: '85714312312',
+    imagen: '/images/baby.png',
+    descripcion: 'Si te has decidido a tener un perro, seguro que antes de comprar –o mejor, adoptar- a tu nueva mascota, tendrás qué decidir qué raza prefieres en función de tu modo de vida y tu propia personalidad. Hay perros de carácter más agresivo o dominante, otros mansos y alegres, algunos necesitan mucho espacio para correr y jugar, otros son más tranquilos…',
+    disponible: true,
+});
+productos.push({
+    titulo: 'Gato',
+    precio: '8571431231200000',
+    imagen: 'https://www.feelcats.com/blog/wp-content/uploads/2018/10/gato-atigrado.jpg',
+    descripcion: 'Esa preciosa combinación de patrones jaspeados, moteados o rayados en diferentes tonos, nos da a la idea de una genética caprichosa, que te contaremos, en este original artículo sobre ellos, pero además nos adentraremos en su carácter y peculiaridades, de estos gatos atigrados tan comunes, pero a la vez, tan únicos. ¿empezamos?',
+    disponible: false,
+});
+
+// configurar la ruta inicial
+
+// configurar la ruta portafolio
+app.get('/tiendanp', function(request, response){
+    response.send('Mi tienda');
+});
+
+// iniciar el servidor en el puerto 3000
+app.listen(3000, function() {
+  console.log('Aplicación ejemplo, escuchando el puerto 3000!');
+});
