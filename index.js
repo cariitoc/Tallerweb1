41b5af6eb1ad4c9415204c0d3e7b2d7ea2730562
Carolina Cucuname Pechene <1144102654@academico.icesi.edu.co>
2019-05-15 14:15:25 -0500

    actualizacion de estilos

--- index.js
+++ index.js
@@ -18,13 +18,13 @@ const MongoClient = require("mongodb").MongoClient;
 const test = require("assert");
 // Connection url
 const url = "mongodb://localhost:27017";
-// Database Name
+// db Name
 const dbName = "tienda";
-var database;
+var db;
 
 // Connect using MongoClient
-MongoClient.connect(url, function(err, client) {
-    database = client.db(dbName);
+MongoClient.connect(url, function (err, client) {
+  db = client.db(dbName);
 
   //client.close();
 });
@@ -33,38 +33,121 @@ MongoClient.connect(url, function(err, client) {
 
 var productos = [];
 productos.push({
-    titulo: 'nevermine',
-    precio: '85714312312',
-    imagen: '/images/baby.png',
-    descripcion: 'Si te has decidido a tener un perro, seguro que antes de comprar –o mejor, adoptar- a tu nueva mascota, tendrás qué decidir qué raza prefieres en función de tu modo de vida y tu propia personalidad. Hay perros de carácter más agresivo o dominante, otros mansos y alegres, algunos necesitan mucho espacio para correr y jugar, otros son más tranquilos…',
-    disponible: true,
+  titulo: 'nevermine',
+  precio: '85714312312',
+  imagen: '/images/baby.png',
+  descripcion: 'Si te has decidido a tener un perro, seguro que antes de comprar –o mejor, adoptar- a tu nueva mascota, tendrás qué decidir qué raza prefieres en función de tu modo de vida y tu propia personalidad. Hay perros de carácter más agresivo o dominante, otros mansos y alegres, algunos necesitan mucho espacio para correr y jugar, otros son más tranquilos…',
+  disponible: true,
 });
 productos.push({
-    titulo: 'Gato',
-    precio: '8571431231200000',
-    imagen: 'https://www.feelcats.com/blog/wp-content/uploads/2018/10/gato-atigrado.jpg',
-    descripcion: 'Esa preciosa combinación de patrones jaspeados, moteados o rayados en diferentes tonos, nos da a la idea de una genética caprichosa, que te contaremos, en este original artículo sobre ellos, pero además nos adentraremos en su carácter y peculiaridades, de estos gatos atigrados tan comunes, pero a la vez, tan únicos. ¿empezamos?',
-    disponible: false,
+  titulo: 'Gato',
+  precio: '8571431231200000',
+  imagen: 'https://www.feelcats.com/blog/wp-content/uploads/2018/10/gato-atigrado.jpg',
+  descripcion: 'Esa preciosa combinación de patrones jaspeados, moteados o rayados en diferentes tonos, nos da a la idea de una genética caprichosa, que te contaremos, en este original artículo sobre ellos, pero además nos adentraremos en su carácter y peculiaridades, de estos gatos atigrados tan comunes, pero a la vez, tan únicos. ¿empezamos?',
+  disponible: false,
 });
 
 // configurar la ruta inicial
 
 // configurar la ruta portafolio
-app.get('/tienda', function(request, response){
+app.get('/tienda', function (request, response) {
 
-    
-  let coleccion = database.collection("productos");
-  
-  coleccion.find({}).toArray(function(err, items) {
+  let contexto = {};
+  let coleccion = db.collection("productos");
+
+  coleccion.find({}).toArray(function (err, items) {
     test.equal(null, err);
-    items.forEach(function(item) {
-      console.log(item);
-    });
+    contexto.productos = items;
+    response.render('tienda', contexto);
   });
-    response.render('tienda', productos[0]);
+
+});
+
+app.get('/temporada/:item?', function (request, response) {
+
+  let contexto = {};
+  let item = request.params.item;
+
+  let query = {};
+
+  if(item != null){
+    query.vendidos = item;
+  }
+
+  let coleccion = db.collection("productos");
+
+  coleccion.find(query).toArray(function (err, items) {
+    test.equal(null, err);
+    contexto.productos = items;
+    response.render('tienda', contexto);
+  });
+
+});
+
+app.get('/precio/:item?', function (request, response) {
+
+  let contexto = {};
+  let item = request.params.item;
+
+  let query = {};
+
+  if(item != null){
+    query.precio = item;
+  }
+
+  let coleccion = db.collection("productos");
+
+  coleccion.find(query).toArray(function (err, items) {
+    test.equal(null, err);
+    contexto.productos = items;
+    response.render('tienda', contexto);
+  });
+
+});
+
+app.get('/categoria/:item?', function (request, response) {
+
+  let contexto = {};
+  let item = request.params.item;
+
+  let query = {};
+
+  if(item != null){
+    query.categoria = item;
+  }
+
+  let coleccion = db.collection("productos");
+
+  coleccion.find(query).toArray(function (err, items) {
+    test.equal(null, err);
+    contexto.productos = items;
+    response.render('tienda', contexto);
+  });
+
+});
+
+app.get('/producto/:item?', function (request, response) {
+
+  let contexto = {};
+  let item = request.params.item;
+
+  let query = {};
+
+  if(item != null){
+    query.nombre = item;
+  }
+
+  let coleccion = db.collection("productos");
+
+  coleccion.find(query).toArray(function (err, items) {
+    test.equal(null, err);
+    contexto = items;
+    response.render('producto', contexto[0]);
+  });
+
 });
 
 // iniciar el servidor en el puerto 3000
-app.listen(3000, function() {
+app.listen(3000, function () {
   console.log('Aplicación ejemplo, escuchando el puerto 3000!');
 });
